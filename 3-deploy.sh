1a59d1c9cd35407d80912e696c08ee9df4394440
Greg Walker <greg_walker@sympatico.ca>
2021-02-26 21:33:45 +0000

    Passing in spreadsheet to work from
    Update script to require name of spreadsheet to work from, so can copy it to code dir and include in lambda upload.

--- 3-deploy.sh
+++ 3-deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -eo pipefail
-ARTIFACT_BUCKET=$(cat code/bucket-name.txt)
-aws cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
-aws cloudformation deploy --template-file out.yml --stack-name stock-price-retriever --capabilities CAPABILITY_NAMED_IAM
+if [ $# == 0 ]
+then
+	echo "Please provide path to spreadsheet to upload"
+else
+	cp $1 code/
+	ARTIFACT_BUCKET=$(cat code/bucket-name.txt)
+	aws cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
+	aws cloudformation deploy --template-file out.yml --stack-name stock-price-retriever --capabilities CAPABILITY_NAMED_IAM
+fi
