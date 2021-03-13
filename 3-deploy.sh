753941ec92f9c6fbe4f91eb88284e7b44c639ce4
Greg Walker <greg_walker@sympatico.ca>
2021-03-13 14:30:16 +0000

    Fixed deploy script
    Deploy script was copying requested spreadsheet to the code dir, but not renaming it to the required filename. Corrected.

--- 3-deploy.sh
+++ 3-deploy.sh
@@ -4,7 +4,7 @@ if [ $# == 0 ]
 then
 	echo "Please provide path to spreadsheet to upload"
 else
-	cp $1 code/
+	cp $1 code/Portfolio.xls
 	ARTIFACT_BUCKET=$(cat code/bucket-name.txt)
 	aws cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
 	aws cloudformation deploy --template-file out.yml --stack-name stock-price-retriever --capabilities CAPABILITY_NAMED_IAM
