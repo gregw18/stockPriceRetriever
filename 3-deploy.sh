3ac159d9d38e67a477e4536fd6f0ef90a8f795d9
Greg Walker <greg_walker@sympatico.ca>
2023-01-06 15:50:09 -0500

    Updated deploy script Don't require spreadsheet parameter any more, as using database for list of securities, rather than a spreadsheet.

--- 3-deploy.sh
+++ 3-deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eo pipefail
-if [ $# == 0 ]
-then
-	echo "Please provide path to spreadsheet to upload"
-else
-	cp $1 src/code/Portfolio.xls
+#if [ $# == 0 ]
+#then
+#	echo "Please provide path to spreadsheet to upload"
+#else
+#	cp $1 src/code/Portfolio.xls
 	ARTIFACT_BUCKET=$(cat src/code/bucket-name.txt)
 	aws cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
 	aws cloudformation deploy --template-file out.yml --stack-name stock-price-retriever --capabilities CAPABILITY_NAMED_IAM \
