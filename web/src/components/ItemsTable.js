828209f18e8e4d3dc01637ce48831f395496c494
Greg Walker <greg_walker@sympatico.ca>
2023-01-12 11:27:17 -0500

    Moved tableRows Moved logic to generate row for each security to new ItemsRows component.

--- web/src/components/ItemsTable.js
+++ web/src/components/ItemsTable.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
-import {Chart, GainChart, TimeRangeChart, BuySellChart, PriceHistoryChart} from "./Chart";
 //import './App.css';
 import { getFakeData } from "./FakeData";
+import { ItemsRows } from "./ItemsRows";
 import { TimePeriodButtons } from "./TimePeriodButtons";
 
 const api_endpoint = "https://n7zmmbsqxc.execute-api.us-east-1.amazonaws.com/Prod/data";
@@ -166,7 +166,8 @@ export class ItemsTable extends React.Component {
               </tr>
             </thead>
             <tbody>
-              {this.generateRows(this.state.securityData)}
+              <ItemsRows
+                securityData={this.state.securityData} />
             </tbody>
           </table>
         </div>
@@ -174,29 +175,6 @@ export class ItemsTable extends React.Component {
     }
     console.log(new Date().toTimeString(), "render complete, took ", performance.now() - t5, " ms.");
     return myHtml;
-  }
-
-  generateRows(mySecurityData) {
-    console.log("generateRows, data=", mySecurityData);
-    let myMinMaxGain = getMinMaxGain(mySecurityData);
-    return (
-      <>
-        {mySecurityData.map(
-          d => (
-            <tr>
-              <td>{d.data.name}</td>
-              <td>{d.data.group}</td>
-              <td>{d.data.rating}</td>
-              <td>{d.data.currentPrice}</td>
-              <td><GainChart chartData={d.data} minMax={myMinMaxGain} /></td>
-              <td><TimeRangeChart chartData={d.data} /></td>
-              <td><BuySellChart chartData={d.data} /></td>
-              <td><PriceHistoryChart chartData={d.data} /></td>
-            </tr>
-            )
-        )}
-      </>
-    );
   }
 
   handleChangeTimePeriod(newPeriod) {
@@ -254,34 +232,5 @@ export class ItemsTable extends React.Component {
     })
     console.log("sortByCol took ", performance.now() - t1, " ms.");
   }
-}
-
-
-// Get lowest and highest percent gains from given set of securities.
-function getMinMaxGain(myChartData) {
-  const t3 = performance.now();
-  let minGain = 9999;
-  let maxGain = -9999;
-  myChartData.forEach(function (security) {
-    if (security.data.percentGain < minGain ) {
-      minGain = security.data.percentGain;
-    }
-    if (security.data.percentGain > maxGain) {
-      maxGain = security.data.percentGain;
-    }
-  });
-
-  // Want range (gain resolution) to be a multiple of five.
-  let gainres = 5;
-  minGain = Math.floor(minGain/gainres) * gainres;
-  maxGain = Math.ceil(maxGain/gainres) * gainres;
-
-  // If max gain is less than zero, or min gain is greater than zero, want
-  // range to include zero.
-  minGain = (minGain > 0 ? 0 : minGain);
-  maxGain = (maxGain < 0 ? 0 : maxGain);
-  console.log("ran getMinMaxGain, returning: ", minGain, ", ", maxGain)
-  console.log(new Date().toTimeString(), "getMinMaxGain took ", performance.now() - t3, " ms.");
-  return {minGain: minGain, maxGain: maxGain};
 }
 
