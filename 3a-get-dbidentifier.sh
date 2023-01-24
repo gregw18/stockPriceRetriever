858eeee27486beca52de72204b9e69730ff4b1ba
Greg Walker <greg_walker@sympatico.ca>
2023-01-24 15:57:47 -0500

    Fixed output of new script Corrected dbidentifier script to put correct value in output file.

--- 3a-get-dbidentifier.sh
+++ 3a-get-dbidentifier.sh
@@ -1,5 +1,5 @@
-url=$(aws rds describe-db-instances \
+id=$(aws rds describe-db-instances \
     --query 'DBInstances[0].[DBInstanceIdentifier]' \
     --output text)
 # Write endpoint to file for code to use.
-echo https://dev.$url > dbidentifier.txt
+echo $id > dbidentifier.txt
